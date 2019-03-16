f6d48bed5459ee50c15d55f521024b63bb1fbbc7
Kim Wysocki <kcwysocka@icloud.com>
2019-03-16 13:00:09 +0000

    Using Regex to add random characters
    - Created a var holding the potential characters to insert into user input string
    - Created var 'newPassword', using regex & Math.random to insert a random character after each character from the input
    
    - *Issue* - the 'random' character that is added is the same character repeated, but I want these to be different characters each time, otherwise it's not very secure

--- password.js
+++ password.js
@@ -1,7 +1,17 @@
-
 function getInput() {
+    // Getting string from user input
     var newInput = document.getElementById("user-input").value;
-    document.getElementById("strongPassword").textContent = newInput;
     document.getElementById("user-input").value = null;
+    
+    // Setting up possible characters
+    var possChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZxyz0123456789!@£#$%^&*?";
+
+    // Inserting random characters into user input
+    var newPassword = newInput.replace(/(.{1})/g, "$1" + possChars.charAt(Math.floor(Math.random() * possChars.length)));
+    
+    // return passwordArray
+    document.getElementById("strongPassword").textContent = newPassword;
+    
   }
+
 
